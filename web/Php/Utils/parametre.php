c483c4fd07e8c451e54275628326a3c137512eaf
manelbelaidouni <belaidouni.manel26@gmail.com>
2025-06-18 21:37:45 +0200

    Delete web/Php/Utils/parametre.php

--- web/Php/Utils/parametre.php
+++
@@ -1,9 +0,0 @@
-<?php
-// parametre.php
-
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3306');
-define('DB_NAME', 'appli');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('SQL_app','mysql');
